d614f5104e787b8fdd7d7a03c46f9fafa21ad487
Vladyslav Tyshchenko <Dvados9989@gmail.com>
2024-07-19 14:52:28 -0600

    Quick update

--- contracts/ERC20/ERC20.sol
+++ contracts/ERC20/ERC20.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {IERC20} from "./IERC20.sol";
-import {IERC20Metadata} from "./IERC20Metadata.sol";
+import {IERC20Metadata} from "./extensions/IERC20Metadata.sol";
 
 abstract contract ERC20 is IERC20, IERC20Metadata {
     string public name;
